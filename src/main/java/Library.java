152c208d7279df367016d83d740e335d02ff72cb
DeuterGraves <carolinetgraves@gmail.com>
2018-11-20 19:36:14 +0000

    library tests all passing. mvp met.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,35 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private int capacity;
+    private ArrayList<Book> collection;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        this.collection = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public ArrayList<Book> getCollection() {
+        return collection;
+    }
+
+
+    public int bookCount() {
+        return collection.size();
+    }
+
+    public void addBook(Book book){
+        if (this.bookCount() < this.capacity){
+        collection.add(book);
+        }
+        return;
+    }
+
+
+
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,10 +1,57 @@
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
 
-        /*
+//Library has an array of books
+    private Book book1;
+    private Book book2;
+    private Book book3;
+    private Book book4;
+    private Library library;
 
-Write a method to add a book to the library stock.
-Write a method to count the number of books in the library.
-Add a capacity to the library and write a method to check if stock is full before adding a book.
+    @Before
+    public void before(){
+        book1 = new Book("The Tango Singer", "Tomas Eloy Martinez", "fiction");
+        book2 = new Book("Romeo & Juliet", "David Hewson", "fiction");
+        book3 = new Book("If I Ever Get Out of this World Alive", "Steve Earle", "noir fiction");
+        book4 = new Book("Carsick", "John Waters", "non-fiction");
+        library = new Library(3);
+    }
 
-     */
+    //Write a method to count the number of books in the library.
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.bookCount());
+    }
+
+//Write a method to add a book to the library stock.
+
+    @Test
+    public void booksCanBeAddedToLibrary(){
+        library.addBook(book1);
+        assertEquals(1, library.bookCount());
+    }
+
+//Add a capacity to the library and write a method to check if stock is full before adding a book.
+
+    @Test
+    public void libraryHasACapacity(){
+        assertEquals(3, library.getCapacity());
+    }
+
+//    library won't go over capacity
+    @Test
+    public void libraryWontOverFlow(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        library.addBook(book4);
+        assertEquals(3, library.bookCount());
+    }
+
 }
